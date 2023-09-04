5c1b41cfc8f3418cccc4286ac88106b73303a6cf
Scheff__ <sandro.niederhauser@gmail.com>
2023-09-04 14:19:05 +0200

    change gcal auth

--- gcal/gcal.py
+++ gcal/gcal.py
@@ -11,6 +11,7 @@ import pickle
 import os.path
 import pathlib
 from googleapiclient.discovery import build
+from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 import logging
@@ -25,12 +26,11 @@ class GcalHelper:
         self.currPath = str(pathlib.Path(__file__).parent.absolute())
 
         creds = None
-        # The file token.pickle stores the user's access and refresh tokens, and is
+        # The file token.json stores the user's access and refresh tokens, and is
         # created automatically when the authorization flow completes for the first
         # time.
-        if os.path.exists(self.currPath + '/token.pickle'):
-            with open(self.currPath + '/token.pickle', 'rb') as token:
-                creds = pickle.load(token)
+        if os.path.exists(self.currPath + '/token.json'):
+            creds = Credentials.from_authorized_user_file(self.currPath + '/token.json', SCOPES)
         # If there are no (valid) credentials available, let the user log in.
         if not creds or not creds.valid:
             if creds and creds.expired and creds.refresh_token:
@@ -40,8 +40,9 @@ class GcalHelper:
                     self.currPath + '/credentials.json', SCOPES)
                 creds = flow.run_local_server(port=0)
             # Save the credentials for the next run
-            with open(self.currPath + '/token.pickle', 'wb') as token:
-                pickle.dump(creds, token)
+            with open(self.currPath + '/token.json', 'w') as token:
+                token.write(creds.to_json())
+
 
         self.service = build('calendar', 'v3', credentials=creds, cache_discovery=False)
 
